64963ca0e3c7b0802a3299ed7348aaace4ab0884
Atayağız Uslu <57071570+Atayazz@users.noreply.github.com>
2023-07-19 14:51:14 +0300

    user
    created superuser

--- backend/Blog_Website/settings.py
+++ backend/Blog_Website/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog_api',
-    "corsheaders"
-    'rest_framework'
+    "corsheaders",
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
