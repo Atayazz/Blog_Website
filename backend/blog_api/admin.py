42aef4debabb734a52882cd3ab8f88a01494d8f0
Atayağız Uslu <57071570+Atayazz@users.noreply.github.com>
2023-07-19 14:44:20 +0300

    models
    models added to the project

--- backend/blog_api/admin.py
+++ backend/blog_api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Tag, Author, Post
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Author)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'pub_date')
+    list_filter = ('author', 'categories', 'tags')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
--- backend/blog_api/models.py
+++ backend/blog_api/models.py
@@ -1,4 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    tags = models.ManyToManyField(Tag)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
